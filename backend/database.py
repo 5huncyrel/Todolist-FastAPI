b0f1bc5867d9012ea59b2d1fe53a7b45dba45c08
Shun Cyrel <caseres.shun@gmail.com>
2025-04-07 18:05:00 +0800

    Backend changes

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://todolist_django_db_user:ZaJ7Qc4jnasQMt5iIeWTgxyBqZcGQi5A@dpg-cvlvboripnbc73bfg5cg-a.oregon-postgres.render.com/todolist_django_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
