aaf022955bb98d09c915270807c6fca47a7aca2b
Shun Cyrel <caseres.shun@gmail.com>
2025-04-07 19:27:52 +0800

    Backend changes

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 # Allow frontend from Vite
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173", 
+    "https://todo-fastapi-6424a.web.app"  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
