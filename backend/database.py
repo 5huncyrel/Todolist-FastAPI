2b5433aee8f0af10fb471a9535dbb4cf0426705f
5huncyrel <158429502+5huncyrel@users.noreply.github.com>
2025-05-25 18:40:49 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://todolist_django_db_user:ZaJ7Qc4jnasQMt5iIeWTgxyBqZcGQi5A@dpg-cvlvboripnbc73bfg5cg-a.oregon-postgres.render.com/todolist_django_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://alertfi_db_ytvx_user:P9NMr3B2iY1QeZ6Q3aAjKuwzDl0eZt07@dpg-d0m1m915pdvs738qkrf0-a/alertfi_db_ytvx"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
